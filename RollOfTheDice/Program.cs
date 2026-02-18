66159fb2f454a85ce74598a1bad12201b68c0bf7
cambriajm <cambriajm@CAMDELL>
2026-02-17 17:25:58 -0700

    ough

--- RollOfTheDice/Program.cs
+++ RollOfTheDice/Program.cs
@@ -1,10 +1,38 @@
 ﻿namespace RollOfTheDice
+//Cambria Morgan
+//RCET 2265
+//Spring 2026
+//RollOfTheDice
+//PUT THE URL DUMBASS
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            
+            int randomNumber = 1;
+            int diceNumber = 0;
+            //pause 
+            Console.Read();
+        }
+        static void runDice()
+        {
+
+        }
+        static void diceNumber( int RandomNumber = 1000)
+        {
+            int dice1 = 0, dice2 = 0;
+            do
+            {
+                dice1 = RandomNumber(4);
+                dice2 = RandomNumberZeroTo(14);
+            } while
+
+           
+        }
+
+        static void Display()
+        {
+
         }
     }
 }
