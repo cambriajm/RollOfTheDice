4dc8e94d6e8705d5eee5b1030a8a34b1f7fc41c2
cambriajm <cambriajm@CAMDELL>
2026-02-22 17:21:08 -0700

    fin

--- RollOfTheDice/Program.cs
+++ RollOfTheDice/Program.cs
@@ -3,36 +3,39 @@
 //RCET 2265
 //Spring 2026
 //RollOfTheDice
-//PUT THE URL DUMBASS
+//https://github.com/cambriajm/RollOfTheDice.git
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int randomNumber = 1;
-            int diceNumber = 0;
+            Random random = new Random(); //makes a random number variable
+            int[] dice = new int[13];
+            string format = "|"; // to space out the numbers of rolls 
+            Console.WriteLine("Roll of the dice, rolls a dice 1000 times,"
+                + "and will show how many rolls per number.");
+            for (int i = 0; i < 1000; i++) // array to roll 1000 times 
+            {
+                int n = random.Next(2, 12 + 1);
+                dice[n]++;
+            }
+
+            for (int i = 2; i <= 12; i++) // to show numbers 2-12 
+            {
+                Console.Write($"{i}".PadRight(5) + format);
+            }
+            Console.WriteLine();
+
+            for(int i = 2; i <= 12; i++) // to list how many times 
+            {
+                Console.Write($"{dice[i]}".PadRight(5) + format);
+            }
+            Console.WriteLine();
+
             //pause 
             Console.Read();
         }
-        static void runDice()
-        {
-
-        }
-        static void diceNumber( int RandomNumber = 1000)
-        {
-            int dice1 = 0, dice2 = 0;
-            do
-            {
-                dice1 = RandomNumber(4);
-                dice2 = RandomNumberZeroTo(14);
-            } while
-
-           
-        }
-
-        static void Display()
-        {
-
-        }
     }
 }
+
+
